0d4eb7e7cb3548f9308af12311fe663791a5df3d
Joy Brar <joybrar2001@gmail.com>
2024-09-27 18:59:45 +0530

    UI Done

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import SearchInput from './SearchInput'
+import Conversations from './Conversations'
+import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <SearchInput />
-      <div className='divider px-3'></div>
+      <div className="divider px-3" />
+      <Conversations />
+      <LogoutButton />
     </div>
   )
 }
