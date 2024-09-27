012ffa55e78ac59db770f1b3bdcd335a09b2d9c0
Joy Brar <joybrar2001@gmail.com>
2024-09-28 00:13:12 +0530

    Done with auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import Homepage from './pages/home/HomePage'
+import SignUpPage from './pages/signup/SignUpPage'
+import LoginPage from './pages/login/LoginPage'
+import { useAuthContext } from './context/AuthContext'
 
 const App = () => {
+  const { authUser } = useAuthContext();
+
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path={"/"} element={<Homepage />} />
-        <Route path={"/login"} element={<LoginPage />} />
-        <Route path={"/signup"} element={<SignUpPage />} />
+        <Route path={"/"} element={authUser ? <Homepage /> : <Navigate to="/login" />} />
+        <Route path={"/login"} element={authUser ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path={"/signup"} element={authUser ? <Navigate to="/" /> : <SignUpPage />} />
       </Routes>
       <Toaster />
     </div>
--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, loading } = useLogin();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -12,7 +23,7 @@ const LoginPage = () => {
           </span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">
@@ -24,6 +35,8 @@ const LoginPage = () => {
               type="text"
               placeholder="Enter Username"
               className="w-full input input-bordered h-10"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
@@ -38,6 +51,8 @@ const LoginPage = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
@@ -47,13 +62,13 @@ const LoginPage = () => {
 
           <div>
             <button className="btn btn-block btn-sm mt-2">
-              Login
+              {loading ? <span className="loading loading-spinner"></span> : "Login"}
             </button>
           </div>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
